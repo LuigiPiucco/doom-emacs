dabc3972ca03ac526d8a77caee53169f25a49b11
Luigi Sartor Piucco <luigipiucco@gmail.com>
2023-07-27 15:06:29 -0300

    feat(corfu): move orderless filtering behing flag
    This allows to setup orderless regardless of :completion vertico. It
    permits conbinations like helm + corfu to still use the sorting.

--- modules/completion/corfu/packages.el
+++ modules/completion/corfu/packages.el
@@ -7,3 +7,5 @@
   (package! kind-icon :pin "3fedd3054e9148fc0a13a06b267351fce6a385b1"))
 (when (modulep! :os tty)
   (package! corfu-terminal :pin "5ce4c11b8efd4d2fd1b404b9422bb85b05476da0"))
+(when (modulep! +orderless)
+  (package! orderless :pin "e6784026717a8a6a7dcd0bf31fd3414f148c542e"))
